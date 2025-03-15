55209df4f86be59b219504a8a01c34ce368bbb9d
Will Santana <fabulasix6@hotmail.com>
2025-03-15 07:01:03 -0300

    ExerciciosCursoemPython

---
+++ desafio33.py
@@ -0,0 +1,8 @@
+n1 = int(input('Digite um primeiro numero: '))
+n2 = int(input('Digite um segundo numero: '))
+n3 = int(input('Digite um terceiro numero: '))
+maior = max(n1, n2, n3)
+menor = min(n1, n2, n3)
+
+print(f'O maior número é {maior}')
+print(f'O menor número é {menor}')
